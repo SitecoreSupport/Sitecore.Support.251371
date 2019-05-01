60607759ab4c86841c92d4c4d1c59046e2587aa3
DK\pfi <pfi@sitecore.net>
2019-05-01 11:21:01 -0400

    Initial Commit

---
+++ src/Sitecore.Support.251371/Analytics/Pipelines/EndAnalytics/ExcludeUrls.cs
@@ -0,0 +1,41 @@
+﻿namespace Sitecore.Support.Analytics.Pipelines.EndAnalytics
+{
+  using Sitecore.Analytics;
+  using Sitecore.Diagnostics;
+  using Sitecore.Pipelines;
+  using System.Collections.Generic;
+  using System.Web;
+  using System.Xml;
+  using Sitecore.Xml;
+  using System;
+
+  public class ExcludeUrls
+  {
+    private readonly List<string> startUrls = new List<string>();
+
+    public virtual void AddStartUrl(XmlNode configNode)
+    {
+      Assert.ArgumentNotNull(configNode, "configNode");
+      string path = XmlUtil.GetAttribute("path", configNode);
+      startUrls.Add(path);
+    }
+
+    public virtual void Process(PipelineArgs args)
+    {
+      if (HttpContext.Current != null && HttpContext.Current.Request != null &&
+          HttpContext.Current.Request.FilePath != null)
+      {
+        foreach (var startUrl in startUrls)
+        {
+          if (HttpContext.Current.Request.FilePath.StartsWith(startUrl, StringComparison.OrdinalIgnoreCase))
+          {
+            if (Tracker.Current != null && Tracker.Current.CurrentPage != null)
+            {
+              Tracker.Current.Interaction.CurrentPage.Cancel();
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.251371/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.251371")]
+[assembly: AssemblyProduct("Sitecore.Support.251371")]
+[assembly: ComVisible(false)]
